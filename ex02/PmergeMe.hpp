6791291fba7c54df9b4040953d623506bf71568c
veron <ver0niqueg@LAPTOP-O1EHI99F.localdomain>
2025-10-21 00:40:12 +0200

    to be continued

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 22:28:08 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/10/20 22:28:08 by vgalmich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <sstream>
+#include <stdexcept>
+#include <ctime>
+#include <sys/time.h>
+#include <climits>
+#include <iomanip>
+
+class PmergeMe
+{
+	private:
+		std::vector<int> _vectorData;
+		std::deque<int> _dequeData;
+
+		// parsing
+		bool isValidNumber(const std::string& str);
+		void parseInput(int argc, char** argv);
+
+		// vector
+		void fordJonhsonVector(std::vector<int>& arr);
+		void insertionVector(std::vector<int>& arr, int left, int right);
+		void mergeVector(std::vector<int>& arr, int left, int mid, int right);
+
+		// deque
+		void fordJonhsonDeque(std::deque<int>& arr);
+		void insertionDeque(std::deque<int>& arr, int left, int right);
+		void mergeDeque(std::deque<int> arr, int left, int mid, int right);
+
+		void printSequence(const std::string& prefic, const std::vector<int>& data, size_t maxDisplay);
+
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe &copy);
+		PmergeMe& operator=(const PmergeMe &other);
+		~PmergeMe();
+
+		void run(int argc, char** argv);
+};
+
+#endif
